3fb2738d57d44e3ff67be1b65bf0e781f47b57b5
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-03-24 14:41:57 +0000

    removed services for now

--- src/init/main.cpp
+++ src/init/main.cpp
@@ -1,15 +1,10 @@
-#include <pbus/servicemanager/Service.h>
-#include <pbus/servicemanager/ServiceRegistry.h>
+#include <pbus/ServiceId.h>
 #include <pbus/LocalBus.h>
 
 int main(int argc, char **argv)
 {
 	using namespace pbus;
-	ServiceId serviceId("ServiceManager");
-	ServiceRegistry registry;
-	ServiceDescriptor initDescriptor;
-	initDescriptor.Executable = argv[0];
-	registry.Add(serviceId, std::move(initDescriptor));
+	ServiceId serviceId("system.core.ServiceManager");
 	LocalBus bus(serviceId);
 	while(true)
 		bus.Wait();
