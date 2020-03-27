7eba77fff45cf4d03be0353f41a4a16fea67c27f
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-03-27 17:46:27 +0000

    remove huge if

--- src/lib/pbus/Session.cpp
+++ src/lib/pbus/Session.cpp
@@ -14,30 +14,28 @@ namespace pbus
 	{
 		_log.Debug() << "connecting to " << serviceId;
 		auto i = _connections.find(serviceId);
-		if (i == _connections.end())
-		{
-			try
-			{
-				auto connection = std::make_shared<LocalBusConnection>(serviceId);
-				_connections.insert(std::make_pair(serviceId, connection));
-				return connection;
-			}
-			catch(const std::exception & ex)
-			{
-				_log.Debug() << "exception while connection: " << ex.what();
-				if (serviceId == idl::system::IServiceManager::ClassId)
-					throw Exception("cannot connect to ServiceManager to create service " + serviceId.ToString());
-
-				_log.Debug() << "connecting to service manager at " << idl::system::IServiceManager::ClassId;
-				auto serviceManager = GetService<idl::system::IServiceManager>();
-				auto lease = serviceManager->start(serviceId.Name, serviceId.Version);
-				auto connection = std::make_shared<LocalBusConnection>(serviceId);
-				_connections.insert(std::make_pair(serviceId, connection));
-				return connection;
-			}
-		}
-		else
+		if (i != _connections.end())
 			return i->second;
+
+		try
+		{
+			auto connection = std::make_shared<LocalBusConnection>(serviceId);
+			_connections.insert(std::make_pair(serviceId, connection));
+			return connection;
+		}
+		catch(const std::exception & ex)
+		{
+			_log.Debug() << "exception while connection: " << ex.what();
+			if (serviceId == idl::system::IServiceManager::ClassId)
+				throw Exception("cannot connect to ServiceManager to create service " + serviceId.ToString());
+
+			_log.Debug() << "connecting to service manager at " << idl::system::IServiceManager::ClassId;
+			auto serviceManager = GetService<idl::system::IServiceManager>();
+			auto lease = serviceManager->start(serviceId.Name, serviceId.Version);
+			auto connection = std::make_shared<LocalBusConnection>(serviceId);
+			_connections.insert(std::make_pair(serviceId, connection));
+			return connection;
+		}
 	}
 
 }
