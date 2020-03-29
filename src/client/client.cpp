6c0cc725969fd04f70fe78a06e388bbf4be40b86
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-03-29 14:35:32 +0100

    add application for sample client

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,6 +1,7 @@
 #include <pbus/LocalBusConnection.h>
 #include <pbus/ClassId.h>
 #include <pbus/Session.h>
+#include <pbus/Application.h>
 #include <pbus/idl/system/IRandomGenerator.h>
 #include <toolkit/log/Logger.h>
 #include <stdio.h>
@@ -8,6 +9,7 @@
 int main(int argc, char ** argv)
 {
 	using namespace pbus;
+	Application app(ServiceId("app.pbus.example.random"));
 	log::Logger logger("client");
 
 	auto random = Session::Get().GetService<pbus::idl::system::IRandomGenerator>();
