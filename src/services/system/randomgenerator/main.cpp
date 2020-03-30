b0853246fe28a174e9ca3a613452dada23ee905d
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-03-30 11:04:40 +0100

    implement system.RandomGenerator

--- src/services/system/randomgenerator/main.cpp
+++ src/services/system/randomgenerator/main.cpp
@@ -1,9 +1,22 @@
 #include <pbus/Application.h>
+#include <pbus/idl/system/IRandomGenerator.h>
+#include <stdlib.h>
+#include <math.h>
+
+namespace pbus { namespace
+{
+	class RandomGenerator : public idl::system::IRandomGenerator
+	{
+		uint getInteger(uint max)
+		{ return floor(1.0 * rand() / RAND_MAX * max); }
+	};
+}}
 
 int main(int argc, char **argv)
 {
 	using namespace pbus;
-	Application app(ServiceId("system.RandomGenerator"));
+	Application app(RandomGenerator::ClassId);
+	app.RegisterService<RandomGenerator>();
 	app.Run();
 	return 0;
 }
