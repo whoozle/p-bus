690505c4cdd6700445781e80e99d4efeaf1a21cd
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-06-14 21:13:39 +0100

    pass all options to cmake

--- configure.sh
+++ configure.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 rm -rf build
 mkdir build
 pushd build
-cmake .. -DPBUS_DEVEL_MODE:BOOL=ON -DCMAKE_INSTALL_PREFIX:PATH=./rootfs -DCMAKE_BUILD_TYPE:STRING=Debug
-make -j$(nproc) install
+cmake .. -DPBUS_DEVEL_MODE:BOOL=ON -DCMAKE_INSTALL_PREFIX:PATH=./rootfs -DCMAKE_BUILD_TYPE:STRING=Debug $*
 popd
+
+echo
+echo
+echo 'run `make/ninja -C build install` now'
